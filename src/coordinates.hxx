ef39fad10c0e347ac2e97aea46c51876d6f6264c
Yuan Feng <feng1m8@outlook.com>
2025-05-12 13:18:54 +0800

    Update cython 3.1

--- src/coordinates.hxx
+++ src/coordinates.hxx
@@ -1,6 +1,8 @@
 #ifndef YNOGK_CXX_COORDINATES_HXX
 #define YNOGK_CXX_COORDINATES_HXX
 
+#include <ciso646>
+
 #if defined(_MSC_VER) and !defined(_CRT_USE_C_COMPLEX_H)
 #define _CRT_USE_C_COMPLEX_H
 #ifndef _C_COMPLEX_T
@@ -9,6 +11,11 @@ typedef double _Complex _C_double_complex;
 typedef float _Complex _C_float_complex;
 typedef long double _Complex _C_ldouble_complex;
 #endif
+#elif defined(__GLIBCXX__)
+#include <complex>
+#undef __STRICT_ANSI__
+#include <complex.h>
+#define __STRICT_ANSI__ 1
 #endif
 
 #include <complex.h>
